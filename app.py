5e672f62eb5353339be7dccbfe1a456daf813bb9
Mike Vaccaro <michael.r.vaccaro@gmail.com>
2021-05-31 21:45:08 -0500

    add app

---
+++ app.py
@@ -0,0 +1,18 @@
+def sayhi():
+    print("hello")
+    print("to my world")
+    print()
+    print("I am not in the function")
+sayhi()
+
+is_male=True
+if is_male:
+        print("this")
+        print("this")
+        print("that")
+else:
+    print("the other one thing")
+    print("the other two thing")
+print("thatddddd")
+
+
