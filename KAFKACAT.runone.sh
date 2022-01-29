6f67ba9ec2f3d39a4cc77c9694e9b2b7ebedc5ab
michael.vaccaro@chrobinson.com <vaccmicpa@lin2pr2ap23.chrobinson.com>
2022-01-29 09:29:32 -0600

    remove passwpords

--- KAFKACAT.runone.sh
+++ KAFKACAT.runone.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 #CONSUME GET ALL FIELDS
-USERNAME=ds-k8s-logging-user
-USERNAME=filebeat-k8s-ece
-USERNAME=kafkaadmin
 USERNAME=admin
-PASSWORD=TArsy8XrhefW3WV8
-PASSWORD=asdlkfjeoijEE1234!
-PASSWORD="WBxE77qgk46P60(e9"
+PASSWORD=abl;kajsdf
 PASSWORD=ThisIsTheWay
 #above is is for kafkacadmin opprd
 SECCLAUSE="-X security.protocol=SASL_PLAINTEXT -X sasl.mechanisms=SCRAM-SHA-256 -X sasl.username=$USERNAME -X sasl.password=$PASSWORD"
